935952db46329593d79836f583e65bac17f3ab35
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-09-25 15:59:51 -0400

    Add settings and context to track page flow.

--- views_infinite_scroll.js
+++ views_infinite_scroll.js
@@ -1,7 +1,8 @@
 // A stop mechanism to prevent the re-triggering of the scroll event's
 // implementation upon list redraws.
 var _views_infinite_scroll_stop = false;
-var _views_infinite_scroll_pages_allowed = 2;
+
+var _views_infinite_scroll_context = {};
 
 /**
  * Implements hook_install().
@@ -12,11 +13,21 @@ function views_infinite_scroll_install() {
     if (typeof drupalgap.settings.views_infinite_scroll === 'undefined') {
       drupalgap.settings.views_infinite_scroll = {};
     }
-    if (typeof drupalgap.settings.views_infinite_scroll.pages_allowed !== 'undefined') {
-      _views_infinite_scroll_pages_allowed = drupalgap.settings.views_infinite_scroll.pages_allowed;
+    if (typeof drupalgap.settings.views_infinite_scroll.pages_allowed === 'undefined') {
+      drupalgap.settings.views_infinite_scroll.pages_allowed = 2;
     }
   }
   catch (error) { console.log('views_infinite_scroll_install - ' + error); }
+}
+
+/**
+ *
+ */
+function views_infinite_scroll_pages_allowed() {
+  try {
+    return drupalgap.settings.views_infinite_scroll.pages_allowed;
+  }
+  catch (error) { console.log('views_infinite_scroll_pages_allowed - ' + error); }
 }
 
 /**
@@ -48,154 +59,207 @@ $(document).on("scrollstop", function() {
     // If there is no View on the page, don't do anything.
     if (!view[0]) { return; }
     
+    var page_id = drupalgap_get_page_id();
+    
     // Only act on the current page.
-    if (activePage[0].id != drupalgap_get_page_id()) { return; }
+    if (activePage[0].id != page_id) { return; }
+    
+    // Set up the context, if it wasn't already.
+    if (typeof _views_infinite_scroll_context[page_id] === 'undefined') {
+      _views_infinite_scroll_context[page_id] = {
+        down: {
+          last_page: null
+        },
+        up: {
+          last_page: null
+        }
+      };
+    }
 
     /* if total scrolled value is equal or greater
        than total height of content div (total scroll)
        and active page is the target page (pageX not any other page)
        call addMore() function */
+    
+    var direction = null;
     if (scrolled >= scrollEnd) {
+      direction = 'down';
       
       // SCROLLED TO THE BOTTOM...
       
-      console.log('BOTTOM');
-      
-      // If stopping, reset and return.
-      if (_views_infinite_scroll_stop) {
-        _views_infinite_scroll_stop = false;
-        return;
-      }
-      
-      //console.log('activePage: ' + activePage);
-      //console.log('scrolled: ' + scrolled);
-      //console.log('screenHeight: ' + screenHeight);
-      //console.log('contentHeight: ' + contentHeight);
-      //console.log('header: ' + header);
-      //console.log('footer: ' + footer);
-      //console.log('view: ' + view);
-      //console.log('scrollEnd: ' + scrollEnd);
-      
-      var options = _views_embed_view_options;
-      var results = _views_embed_view_results;
-      
-      //console.log('on page ' + (parseInt(results.view.page) + 1) + ' of ' + results.view.pages);
-      
-      // If we're at the last page, stop drop and roll.
-      if (results.view.page == null || results.view.page == results.view.pages - 1) { return; }
-      
-      // Determine the next page.
-      var next_page = parseInt(results.view.page) + 1;
-      
-      // To keep the DOM slim, if we've made it past the second page, we'll
-      // remove the first page. This concept will continue as paging progresses,
-      // essentially only ever having 2 pages in the DOM at the same time.
-      if (next_page >= _views_infinite_scroll_pages_allowed) {
-
-        // Prevent event from firing upon redraw with a stop block.
-        _views_infinite_scroll_stop = true;
-        
-        // Figure out the range of items to remove from the list.
-        //var lower = (next_page - _views_infinite_scroll_pages_allowed) * results.view.limit;
-        //var upper = (next_page - 1) * results.view.limit;
-        var lower = 0;
-        var upper = results.view.limit;
-        var page_to_delete = next_page - _views_infinite_scroll_pages_allowed;
-        console.log('I should delete page ' + page_to_delete + ', from ' + lower + ' to ' + upper);
-        var selector = views_infinite_scroll_selector();
-        for (var i = 0; i < upper; i++) {    
-          var item = $(selector + ' li:eq(0)');
-          if (i == 0 || i == upper - 1) {
-            console.log('remove: ' + $(item).html());  
-          }
-          $(item).remove(); // @TODO probably bad performance here
-        }
-        $(selector).listview();
-        $(selector).listview('refresh');
-      }
-
-      //dpm('options');
-      //console.log(options);
-      //dpm('results');
-      //console.log(results);
-
-      
-      var new_path = options.path + '&page=' + next_page;
-      views_datasource_get_view_result(new_path, {
-          success: function(results) {
-            try {
-              //console.log(results);
-              
-              // Replace the results.
-              _views_embed_view_results = results;
-              _views_embed_view_options.results = results;
-              
-              // Extract the root and child object name.
-              var root = results.view.root;
-              var child = results.view.child;
-              
-              // Render all the rows.
-              var result_formats = drupalgap_views_get_result_formats(_views_embed_view_options);
-              var rows = '' + drupalgap_views_render_rows(
-                _views_embed_view_options,
-                _views_embed_view_results,
-                root,
-                child,
-                result_formats.open_row,
-                result_formats.close_row
-              );
-              
-              // Append the rows html to the results container.
-              // @TODO this only works for ul/ol li's, need to add support for
-              // unformatted and table formats.
-              var selector = views_infinite_scroll_selector();
-              $(selector).append(rows);
-              $(selector).listview();
-              $(selector).listview('refresh');
-              
-              // Unblock the stop, if we we're stopped.
-              if (_views_infinite_scroll_stop) {
-                _views_infinite_scroll_stop = false;
-              }
-    
-            }
-            catch (error) {
-              console.log('views_embed_view - success - ' + error);
-            }
-          }
-      });
 
     }
     else if (scrolled == 0) {
       
+      direction = 'up';
+      
       // SCROLLED TO THE TOP...
-      
-      console.log('TOP');
-      //console.log('activePage: ' + activePage);
-      //console.log('scrolled: ' + scrolled);
-      //console.log('screenHeight: ' + screenHeight);
-      //console.log('contentHeight: ' + contentHeight);
-      //console.log('header: ' + header);
-      //console.log('footer: ' + footer);
-      //console.log('view: ' + view);
-      //console.log('scrollEnd: ' + scrollEnd);
-      
-      // If stopping, reset and return.
-      if (_views_infinite_scroll_stop) {
-        _views_infinite_scroll_stop = false;
-        return;
-      }
-      
-      var options = _views_embed_view_options;
-      var results = _views_embed_view_results;
-      
-      // If we're at the first page, stop drop and roll.
-      if (results.view.page == 0) { return; }
-      
-      // Determine the previous page (aka .
-      var previous_page = results.view.page;
 
     }
+    if (direction === null) { return; }
+      
+    // If stopping, reset and return.
+    if (_views_infinite_scroll_stop) {
+      _views_infinite_scroll_stop = false;
+      return;
+    }
+    
+    console.log(direction);
+    //console.log('activePage: ' + activePage);
+    //console.log('scrolled: ' + scrolled);
+    //console.log('screenHeight: ' + screenHeight);
+    //console.log('contentHeight: ' + contentHeight);
+    //console.log('header: ' + header);
+    //console.log('footer: ' + footer);
+    //console.log('view: ' + view);
+    //console.log('scrollEnd: ' + scrollEnd);
+    
+    var options = _views_embed_view_options;
+    var results = _views_embed_view_results;
+    
+    //console.log('on page ' + (parseInt(results.view.page) + 1) + ' of ' + results.view.pages);
+    
+    // If we're at the last page (or back to the first depending on direction),
+    // stop drop and roll.
+    if (direction == 'down') {
+      if (results.view.page == null || results.view.page == results.view.pages - 1) { return; }
+    }
+    else if (direction == 'up') {
+      if (results.view.page == 0) { return; }
+    }
+    else { return; }
+    
+    
+    // Determine the next page, and hang onto it for the directional context. If
+    // there was an existing context, use it and increment/decrement as needed.
+    console.log(_views_infinite_scroll_context[page_id].up);
+    console.log(_views_infinite_scroll_context[page_id].down);
+    console.log('----');
+    var next_page = null;
+    if (direction == 'down') {
+      next_page = parseInt(results.view.page) + 1;
+      if (_views_infinite_scroll_context[page_id].down.last_page != null) {
+        next_page = _views_infinite_scroll_context[page_id].down.last_page;
+        _views_infinite_scroll_context[page_id].down.last_page = null;
+      }
+      //else {
+        //_views_infinite_scroll_context[page_id].down.last_page = next_page;
+      //}
+    }
+    else if (direction == 'up') {
+      next_page = parseInt(results.view.page) - views_infinite_scroll_pages_allowed();
+      if (_views_infinite_scroll_context[page_id].up.last_page != null) {
+        next_page = _views_infinite_scroll_context[page_id].up.last_page;
+        _views_infinite_scroll_context[page_id].up.last_page = null;
+      }
+      //else {
+        //_views_infinite_scroll_context[page_id].up.last_page = next_page;
+      //}
+    }
+    
+    if (next_page < 0) { next_page = 0; }
+    
+    // Prevent event from firing upon redraw with a stop block.
+    _views_infinite_scroll_stop = true;
+    
+    // Keep the DOM slim by removing items from the top or bottom of the list,
+    // depending on which direction we're scrolling.
+    var lower = null;
+    var upper = null;
+    var page_to_delete = null;
+    var slim = false;
+    if (direction == 'down' && next_page >= views_infinite_scroll_pages_allowed()) {
+
+      // Remove from the top of the list.
+      lower = 0;
+      upper = results.view.limit;
+      page_to_delete = next_page - views_infinite_scroll_pages_allowed();
+      _views_infinite_scroll_context[page_id].up.last_page = page_to_delete;
+      slim = true;
+
+    }
+    else if (direction == 'up') {
+
+      // Remove from the bottom of the list.
+      lower = (next_page + views_infinite_scroll_pages_allowed() - 1) * results.view.limit;
+      upper = lower + results.view.limit;
+      page_to_delete = next_page + views_infinite_scroll_pages_allowed();
+      _views_infinite_scroll_context[page_id].down.last_page = page_to_delete;
+      slim = true;
+
+    }
+    
+    console.log(_views_infinite_scroll_context[page_id].up);
+    console.log(_views_infinite_scroll_context[page_id].down);
+    
+    if (slim) {
+      console.log('I should delete page ' + page_to_delete + ', from ' + lower + ' to ' + upper);
+      var selector = views_infinite_scroll_selector();
+      for (var i = lower; i < upper; i++) {
+        var _selector = selector + ' li:eq(0)';
+        if (direction == 'up') { _selector = selector + ' li:eq(' + lower + ')'; }
+        var item = $(_selector);
+        if (i == lower || i == upper - 1) {
+          console.log('remove: ' + $(item).html());  
+        }
+        $(item).remove(); // @TODO probably bad performance here
+      }
+      $(selector).listview();
+      $(selector).listview('refresh');
+    }
+    
+
+    //dpm('options');
+    //console.log(options);
+    //dpm('results');
+    //console.log(results);
+
+    
+    var new_path = options.path + '&page=' + next_page;
+    views_datasource_get_view_result(new_path, {
+        success: function(results) {
+          try {
+            //console.log(results);
+            
+            // Replace the results.
+            _views_embed_view_results = results;
+            _views_embed_view_options.results = results;
+            
+            // Extract the root and child object name.
+            var root = results.view.root;
+            var child = results.view.child;
+            
+            // Render all the rows.
+            var result_formats = drupalgap_views_get_result_formats(_views_embed_view_options);
+            var rows = '' + drupalgap_views_render_rows(
+              _views_embed_view_options,
+              _views_embed_view_results,
+              root,
+              child,
+              result_formats.open_row,
+              result_formats.close_row
+            );
+            
+            // Append the rows html to the results container.
+            // @TODO this only works for ul/ol li's, need to add support for
+            // unformatted and table formats.
+            var selector = views_infinite_scroll_selector();
+            if (direction == 'down') { $(selector).append(rows); }
+            else { $(selector).prepend(rows); }
+            $(selector).listview();
+            $(selector).listview('refresh');
+            
+            // Unblock the stop, if we we're stopped.
+            if (_views_infinite_scroll_stop) {
+              _views_infinite_scroll_stop = false;
+            }
+  
+          }
+          catch (error) {
+            console.log('views_embed_view - success - ' + error);
+          }
+        }
+    });
 });
 
 /**
@@ -207,3 +271,4 @@ function views_infinite_scroll_selector() {
   }
   catch (error) { console.log('views_infinite_scroll_selector - ' + error); }
 }
+
