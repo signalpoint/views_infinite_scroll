9c71d2d03624d367cdaa07e8393003f89b53f4ba
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-10-19 23:31:45 -0400

    Use global DG views context object. Hang onto last direction moving in the context. Fix paging and slimming mechanisms.

--- views_infinite_scroll.js
+++ views_infinite_scroll.js
@@ -1,5 +1,3 @@
-var _views_infinite_scroll_context = {};
-
 /**
  * Implements hook_install().
  */
@@ -60,19 +58,6 @@ $(document).on("scrollstop", function() {
     // Only act on the current page.
     if (activePage[0].id != page_id) { return; }
 
-    // Set up the context, if it wasn't already.
-    if (typeof _views_infinite_scroll_context[page_id] === 'undefined') {
-      _views_infinite_scroll_context[page_id] = {
-        last_direction: null,
-        down: {
-          last_page: null
-        },
-        up: {
-          last_page: null
-        }
-      };
-    }
-
     /* if total scrolled value is equal or greater
        than total height of content div (total scroll)
        and active page is the target page (pageX not any other page)
@@ -83,13 +68,14 @@ $(document).on("scrollstop", function() {
     if (scrolled >= scrollEnd) { direction = 'down'; } // SCROLLED TO THE BOTTOM...
     else if (scrolled == 0) { direction = 'up'; } // SCROLLED TO THE TOP...
     if (direction === null) { return; }
-    
+
     // Set aside the direction we're moving, if it hasn't been already. Later
     // we'll need to determine if we've switched directions or not.
-    if (!_views_infinite_scroll_context[page_id].last_direction) {
-      _views_infinite_scroll_context[page_id].last_direction = direction;
+    var last_direction = views_embedded_view_get(page_id, 'views_infinite_scroll_last_direction');
+    if (!last_direction) {
+      views_embedded_view_set(page_id, 'views_infinite_scroll_last_direction', direction);
+      last_direction = direction;
     }
-    var last_direction = _views_infinite_scroll_context[page_id].last_direction;
 
     // If stopping, reset and return. A stop mechanism is used to prevent the
     // re-triggering of the scroll event's implementation upon list redraws.
@@ -97,6 +83,10 @@ $(document).on("scrollstop", function() {
       views_embedded_view_set(page_id, 'views_infinite_scroll_stop', false);
       return;
     }
+
+    //console.log('============================');
+
+    // Ok, let's work some magic...
 
     //console.log(direction);
     //console.log('activePage: ' + activePage);
@@ -110,9 +100,9 @@ $(document).on("scrollstop", function() {
 
     var options = views_embedded_view_get(page_id, 'options');
     var results = views_embedded_view_get(page_id, 'results');
-    
+
     //console.log('on page ' + (parseInt(results.view.page) + 1) + ' of ' + results.view.pages);
-    
+
     // If we're at the last page (or back to the first depending on direction),
     // stop drop and roll.
     if (direction == 'down') {
@@ -130,78 +120,60 @@ $(document).on("scrollstop", function() {
     var pages_allowed = views_infinite_scroll_pages_allowed();
     if (direction == 'down') {
       // Switching direction.
-      if (direction != last_direction) {
-        next_page = current_page + pages_allowed;
-      }
+      if (direction != last_direction) { next_page = current_page + pages_allowed; }
       // Same direction.
-      else {
-        next_page = current_page + 1;
-      }
-      dpm('next page down: ' + next_page);
-      //if (_views_infinite_scroll_context[page_id].down.last_page != null) {
-      //  next_page = _views_infinite_scroll_context[page_id].down.last_page;
-      //  _views_infinite_scroll_context[page_id].down.last_page = null;
-      //}
+      else { next_page = current_page + 1; }
+      //dpm('next page down: ' + next_page);
     }
     else if (direction == 'up') {
       // Switching direction.
-      if (direction != last_direction) {
-        next_page = current_page - pages_allowed;
-      }
+      if (direction != last_direction) { next_page = current_page - pages_allowed; }
       // Same direction.
-      else {
-        next_page = current_page - 1;
-      }
-      dpm('next page up: ' + next_page);
-      //if (_views_infinite_scroll_context[page_id].up.last_page != null) {
-      //  next_page = _views_infinite_scroll_context[page_id].up.last_page;
-      //  dpm('next page up context: ' + next_page);
-      //  _views_infinite_scroll_context[page_id].up.last_page = null;
-      //}
+      else { next_page = current_page - 1; }
+      //dpm('next page up: ' + next_page);
     }
+    views_embedded_view_set(page_id, 'views_infinite_scroll_last_direction', direction);
     
     if (next_page < 0) { next_page = 0; }
     
     // Prevent event from firing upon redraw with a stop block.
     views_embedded_view_set(page_id, 'views_infinite_scroll_stop', true);
-    
+
     // Keep the DOM slim by removing items from the top or bottom of the list,
     // depending on which direction we're scrolling.
     var lower = null;
     var upper = null;
-    var page_to_delete = null;
+    var pages_allowed = views_infinite_scroll_pages_allowed();
+    
+    // Let's first figure out if we should slim the DOM or not, and figure out
+    // the upper and lower bounds to the slim.
     var slim = false;
-    if (direction == 'down' && next_page >= views_infinite_scroll_pages_allowed()) {
+    if (direction == 'down' && next_page >= pages_allowed) {
 
       // Remove from the top of the list.
       lower = 0;
       upper = results.view.limit;
-      page_to_delete = next_page - views_infinite_scroll_pages_allowed();
-      dpm('page_to_delete - down : ' + page_to_delete);
-      _views_infinite_scroll_context[page_id].up.last_page = page_to_delete;
       slim = true;
 
     }
     else if (direction == 'up') {
 
       // Remove from the bottom of the list.
-      lower = (next_page + views_infinite_scroll_pages_allowed() - 1) * results.view.limit;
-      upper = lower + results.view.limit;
-      page_to_delete = next_page + views_infinite_scroll_pages_allowed();
-      dpm('page_to_delete - up : ' + page_to_delete);
-      _views_infinite_scroll_context[page_id].down.last_page = page_to_delete;
+      lower = results.view.limit;
+      upper = lower * 2;
       slim = true;
 
     }
-    
+
     if (slim) {
-      //console.log('I should delete page ' + page_to_delete + ', from ' + lower + ' to ' + upper);
       var selector = views_infinite_scroll_selector();
-      console.log('slimming: ' + selector);
+      //console.log('slimming (' + lower + ' => ' + upper + '): ' + selector);
       for (var i = lower; i < upper; i++) {
         var _selector = selector + ' li:eq(0)';
         if (direction == 'up') { _selector = selector + ' li:eq(' + lower + ')'; }
         var item = $(_selector);
+        if (!item) { continue; }
+        // Debug info.
         //if (i == lower || i == upper - 1) {
         //  console.log('remove: ' + $(item).html());  
         //}
@@ -247,7 +219,7 @@ $(document).on("scrollstop", function() {
             // @TODO this only works for ul/ol li's, need to add support for
             // unformatted and table formats.
             var selector = views_infinite_scroll_selector();
-            console.log('expanding (' + direction + '): ' + selector);
+            //console.log('expanding (' + direction + '): ' + selector);
             if (direction == 'down') { $(selector).append(rows); }
             else { $(selector).prepend(rows); }
             $(selector).listview();
